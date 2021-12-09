df1501e4e49b12d70e3d924fa75f1ba93cd11277
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2021-12-09 18:01:31 -0300

    Adding some blocks

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame as pg
-from random import randint
+from random import randint, choice
 
 LARGURA = 650
 ALTURA = 500
 blau = (0, 0, 255)
 weiss = (255, 255, 255)
 rot = (255, 0, 0)
+scharz = (0, 0, 0)
+grun = (0, 0, 0)
 
 
 tamanho = 10
@@ -19,21 +21,26 @@ def jooj():
 
     pos_x = LARGURA/2
     pos_y = ALTURA-30
-    pos_y_car = ALTURA - randint(40, ALTURA - tamanho)
-    pos_x_car = LARGURA - randint(50, LARGURA - tamanho)
+    pos_x_car = LARGURA - tamanho
     tamanho_car = tamanho+randint(5, 10)
     velocidade_x = 0
     clock = pg.time.Clock()
+    ##pos_y_car = ALTURA - randint(40, ALTURA - tamanho)
+    ##pos_x_car = LARGURA - randint(50, LARGURA - tamanho)
+    pos_cars = []
 
+    for i in range(0, 5, 1):
+        pos_cars.append(randint(40, ALTURA - tamanho))
+
+        #pos_cars[i] = i
     fundo = pg.display.set_mode((LARGURA, ALTURA))
     pg.display.set_caption("jooj")
 
-    loop = True
-    while loop:
-        pos_y_car = ALTURA - randint(40, ALTURA - tamanho)
+    while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                loop = False
+                pg.quit()
+                exit()
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_LEFT or event.key == pg.K_a:
                     pos_x -= 10
@@ -47,12 +54,21 @@ def jooj():
                 if event.key == pg.K_DOWN or event.key == pg.K_s:
                     pos_y += 10
 
-        pg.draw.rect(fundo, rot, [pos_x_car, pos_y_car, tamanho_car, tamanho_car])
         fundo.fill(blau)
+
         pg.draw.rect(fundo, weiss, [pos_x, pos_y, tamanho, tamanho])
+
+        pg.draw.rect(fundo, rot, [pos_x_car, choice(pos_cars), tamanho_car, tamanho_car])
+        pg.draw.rect(fundo, weiss, [pos_x_car, choice(pos_cars), tamanho_car, tamanho_car])
+        pg.draw.rect(fundo, scharz, [pos_x_car, choice(pos_cars), tamanho_car, tamanho_car])
+        pg.draw.rect(fundo, grun, [pos_x_car, choice(pos_cars), tamanho_car, tamanho_car])
+        pg.draw.rect(fundo, rot, [pos_x_car, choice(pos_cars), tamanho_car, tamanho_car])
+
         velocidade_x += 0.5
+        pos_x_car -= velocidade_x
+
         pg.display.update()
-        clock.tick(15)
+        clock.tick(5)
 
     pg.quit()
 
